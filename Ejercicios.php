0b1ff34ac79849ca7df8a608afaa580ff6c47c09
Kevin <kcortezm@miumg.edu.gt>
2024-03-02 12:48:26 -0600

    Ejercicios 3 serie primer parcial

---
+++ Ejercicios.php
@@ -0,0 +1,26 @@
+<?php
+
+$sumapar=0;
+$sumaimpar=0;
+ for($i=0; $i<=50; $i++){
+     if($i % 2 ==0){
+         $sumapar += $i;
+
+     }
+     else{
+         $sumaimpar += $i;
+
+     }
+ }
+ echo $sumapar . "\n";
+ echo $sumaimpar . "\n";
+
+
+ $i=0;
+ $numero=33;
+ $repetido=3;
+ while($i<$numero){
+     $i += $repetido ;
+
+ }
+echo $i;
